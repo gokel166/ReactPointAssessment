5c026e71f131abc1591324607e95496b4028b0ad
gokello166 <okellogeorge40@yahoo.com>
2020-09-06 22:29:46 -0500

    test transaction, points and month retrieval data.

--- src/components/PointCalculator.js
+++ src/components/PointCalculator.js
@@ -7,9 +7,24 @@ function CalculateDataOutput(dataSet) {
     const ptPerTransaction = dataSet.map(transaction => {
         let points = 0;
         let aboveOneHundredPts = transaction.amount - 100;
+
+        if (aboveOneHundredPts > 0) {
+            points += (aboveOneHundredPts * 2);
+        }
+        if (transaction.amount > 50) {
+            points += 50;
+        }
+
+        // get month / day / date / year
+        const month = new Date(transaction.transactionDate).getUTCMonth();
+        const day = new Date(transaction.transactionDate).getUTCDay();
+        const date = new Date(transaction.transactionDate).getUTCDate();
+        // console.log(month);
+
+        return { ...transaction, points, month };
     });
 
-    // console.log(ptPerTransaction);
+    console.log(ptPerTransaction);
 
 }
 
--- src/data/testData.js
+++ src/data/testData.js
@@ -2,127 +2,127 @@ export default function() {
     return Promise.resolve(
       [
           {
-            custid: 1,
+            customerId: 1,
             customerName: "Mike",
             amount: 120,
             transactionDate: "05-01-2019"
           },
           {
-            custid: 2,
+            customerId: 2,
             customerName: "Dan",
             amount: 75,
             transactionDate: "05-21-2019"
           },
           {
-            custid: 3,
+            customerId: 3,
             customerName: "George",
             amount: 94,
             transactionDate: "05-21-2020"
           },
           {
-            custid: 4,
+            customerId: 4,
             customerName: "Nigel",
             amount: 10,
             transactionDate: "06-01-2019"
           },
           {
-            custid: 5,
+            customerId: 5,
             customerName: "Brent",
             amount: 75,
             transactionDate: "06-21-2018"
           },
           {
-            custid: 6,
+            customerId: 6,
             customerName: "Cam",
             amount: 200,
             transactionDate: "07-01-2019"
           },
           {
-            custid: 7,
+            customerId: 7,
             customerName: "Harold",
             amount: 1,
             transactionDate: "07-04-2019"
           },
           {
-            custid: 8,
+            customerId: 8,
             customerName: "Joshua",
             amount: 80,
             transactionDate: "07-03-2019"
           },
           {
-            custid: 9,
+            customerId: 9,
             customerName: "Trent",
             amount: 224,
             transactionDate: "07-21-2019"
           },
           {
-            custid: 10,
+            customerId: 10,
             customerName: "Kent",
             amount: 700,
             transactionDate: "05-01-2019"
           },
           {
-            custid: 11,
+            customerId: 11,
             customerName: "Barry",
             amount: 98,
             transactionDate: "05-21-2019"
           },
           {
-            custid: 12,
+            customerId: 12,
             customerName: "Marcus",
             amount: 10,
             transactionDate: "06-01-2019"
           },
           {
-            custid: 13,
+            customerId: 13,
             customerName: "Adrian",
             amount: 75,
             transactionDate: "06-21-2019"
           },
           {
-            custid: 14,
+            customerId: 14,
             customerName: "Luke",
             amount: 200,
             transactionDate: "07-01-2019"
           },
           {
-            custid: 15,
+            customerId: 15,
             customerName: "Simon",
             amount: 224,
             transactionDate: "07-21-2019"
           },
           {
-            custid: 16,
+            customerId: 16,
             customerName: "Aaron",
             amount: 120,
             transactionDate: "06-21-2019"
           },
           {
-            custid: 17,
+            customerId: 17,
             customerName: "David",
             amount: 10,
             transactionDate: "06-01-2019"
           },
           {
-            custid: 18,
+            customerId: 18,
             customerName: "Henry",
             amount: 75,
             transactionDate: "06-21-2019"
           },
           {
-            custid: 19,
+            customerId: 19,
             customerName: "Drake",
             amount: 200,
             transactionDate: "07-01-2019"
           },
           {
-            custid: 20,
+            customerId: 20,
             customerName: "Wayne",
             amount: 224,
             transactionDate: "07-21-2019"
           },
           {
-            custid: 21,
+            customerId: 21,
             customerName: "Chris",
             amount: 120,
             transactionDate: "06-21-2019"
